6546f0b3c86a2fc865774cc3937417ea62a561cf
sgemme <sebastien.gemme@asc-csa.gc.ca>
2024-08-13 15:52:15 -0400

    Typo in pyproject.toml, migrating unit tests to pytest
    Signed-off-by: sgemme <sebastien.gemme@asc-csa.gc.ca>

--- tests/test_codecs.py
+++ tests/test_codecs.py
@@ -1,88 +1,95 @@
-import unittest
+import pytest
 
-from video_tools.types import CodecType,  Codec
-from video_tools.ffmpeg import *
+from videopk.ffmpeg import *
+from videopk.types import Codec, CodecType
 
-class CodecsTestCase(unittest.TestCase):
 
-    def test_list_codecs(self):
-        codecs = Codecs.instance().list_codecs()
+def list_codecs():
+    codecs = Codecs.instance().list_codecs()
 
-        self.assertTrue(len(codecs) > 0)
+    assert len(codecs) > 0
 
-        # Find the hevc codec
-        hevc = [x for x in codecs if x.name == "hevc"]
+    # Find the hevc codec
+    hevc = [x for x in codecs if x.name == "hevc"]
 
-        self.assertTrue(len(hevc) == 1)
+    assert len(hevc) == 1
 
-    def test_parse_line(self):
 
-        hevc = Codecs.parse_codec_line(" DEV.L. hevc                 H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)")
-        
-        self.assertTrue(hevc.encoding)
-        self.assertEqual(hevc.type, CodecType.VIDEO)
-        self.assertTrue(hevc.lossy)
-        self.assertFalse(hevc.lossless)
-        self.assertFalse(hevc.intra_frame_only)
-        self.assertEqual(hevc.name, "hevc")
-        self.assertEqual(hevc.description, "H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)")
+def parse_line():
+    hevc = Codecs.parse_codec_line(
+        " DEV.L. hevc                 H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)"
+    )
 
-        hevc = Codecs.parse_codec_line("DEV.L. hevc                 H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)")
-        self.assertTrue(hevc.encoding)
-        self.assertEqual(hevc.type, CodecType.VIDEO)
-        self.assertTrue(hevc.lossy)
-        self.assertFalse(hevc.lossless)
-        self.assertFalse(hevc.intra_frame_only)
-        self.assertEqual(hevc.name, "hevc")
-        self.assertEqual(hevc.description, "H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)")
+    assert hevc.encoding
+    assert hevc.type == CodecType.VIDEO
+    assert hevc.lossy
+    assert not hevc.lossless
+    assert not hevc.intra_frame_only
+    assert hevc.name == "hevc"
+    assert (
+        hevc.description
+        == "H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)"
+    )
 
-        with self.assertRaises(TypeError):
-            hevc = Codecs.parse_codec_line("test 2")
-       
-    def test_codec(self):
-        codec = Codec()
-        self.assertFalse(codec.decoding)
-        self.assertFalse(codec.encoding)
-        self.assertFalse(codec.intra_frame_only)
-        self.assertEqual(codec.type, CodecType.VIDEO)
-        self.assertFalse(codec.lossy)
-        self.assertFalse(codec.lossless)
-        self.assertEqual(codec.name,"")
-        self.assertEqual(codec.description,"")
+    hevc = Codecs.parse_codec_line(
+        "DEV.L. hevc                 H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)"
+    )
+    assert hevc.encoding
+    assert hevc.type == CodecType.VIDEO
+    assert hevc.lossy
+    assert not hevc.lossless
+    assert not hevc.intra_frame_only
+    assert hevc.name == "hevc"
+    assert (
+        hevc.description
+        == "H.265 / HEVC (High Efficiency Video Coding) (decoders: hevc hevc_v4l2m2m hevc_cuvid) (encoders: hevc_nvenc hevc_v4l2m2m)"
+    )
 
-        codec.name = "test"
-        self.assertEqual(codec.name,"test")
+    with pytest.raises(TypeError):
+        hevc = Codecs.parse_codec_line("test 2")
 
-        codec.description = "un test"
-        self.assertEqual(codec.description,"un test")
 
-        codec.type = CodecType.AUDIO
-        self.assertEqual(codec.type, CodecType.AUDIO)
+def codec():
+    codec = Codec()
+    assert not codec.decoding
+    assert not codec.encoding
+    assert not codec.intra_frame_only
+    assert codec.type == CodecType.VIDEO
+    assert not codec.lossy
+    assert not codec.lossless
+    assert codec.name == ""
+    assert codec.description == ""
 
-        codec.type = CodecType.SUBTITLE
-        self.assertEqual(codec.type, CodecType.SUBTITLE)
+    codec.name = "test"
+    assert codec.name == "test"
 
-        codec.type = CodecType.DATA
-        self.assertEqual(codec.type, CodecType.DATA)
+    codec.description = "un test"
+    assert codec.description == "un test"
 
-        codec = Codec()
-        codec.decoding = True
-        self.assertTrue(codec.decoding)
+    codec.type = CodecType.AUDIO
+    assert codec.type == CodecType.AUDIO
 
-        codec = Codec()
-        codec.encoding = True
-        self.assertTrue(codec.encoding)
+    codec.type = CodecType.SUBTITLE
+    assert codec.type == CodecType.SUBTITLE
 
-        codec.intra_frame_only = True
-        self.assertTrue(codec.intra_frame_only)
+    codec.type = CodecType.DATA
+    assert codec.type == CodecType.DATA
 
-        codec = Codec()
-        codec.lossy = True
-        self.assertTrue(codec.lossy)
+    codec = Codec()
+    codec.decoding = True
+    assert codec.decoding
 
-        codec = Codec()
-        codec.lossless = True
-        self.assertTrue(codec.lossless)
+    codec = Codec()
+    codec.encoding = True
+    assert codec.encoding
 
-if __name__ == "__main__":
-    unittest.main()
+    codec.intra_frame_only = True
+    assert codec.intra_frame_only
+
+    codec = Codec()
+    codec.lossy = True
+    assert codec.lossy
+
+    codec = Codec()
+    codec.lossless = True
+    assert codec.lossless
